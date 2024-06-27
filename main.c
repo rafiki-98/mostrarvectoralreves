89d949692b5de62407b21ad839a0e07fb9428cd6
Kevin Ortiz <107230279+rafiki-98@users.noreply.github.com>
2024-06-27 19:31:12 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int vdato[6]={0,0,0,0,0,0};
+	c=0;
+	
+	for(c=0;c<=5, c++);
+	{
+	  printf("ingrese la posicion %d\n", c);
+	  scandf("%d",%vdato[c]);
+	}
+	
+	for(c=5;c>=0,c--)
+	{
+	  printf("ingrese la posicion %d\n", c);
+	}
+	return 0;
+}
